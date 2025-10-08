9208f6d3b93ea856b4d140adb4d357d80c0ea24f
A <a>
2025-10-08 13:56:14 -0400

    r

--- web/kys.js
+++
@@ -1,9 +0,0 @@
-const app = new PIXI.Application();
-app.init({
-    resizeTo: document.getElementById("game"),
-    autoDensity: true,
-    backgroundColor: 0x1099bb,
-    resolution: window.devicePixelRatio || 1,
-}).then(() => {
-    document.getElementById("game").appendChild(app.canvas);
-});
